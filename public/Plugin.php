9afdf38520d41e0cff3b885d9735c7a0146e58f4
EdwardBock <edward.bock@palasthotel.de>
2020-11-23 18:42:16 +0100

    version update

--- public/Plugin.php
+++ public/Plugin.php
@@ -4,13 +4,13 @@
  * Plugin Name: Live News
  * Plugin URI: https://palasthotel.de
  * Description: Custom post type for news stream
- * Version: 0.1.0
+ * Version: 0.2.0
  * Author: Palasthotel by Stephan <stephan.kroppenstedt@palasthotel.de> & Edward <edward.bock@palasthotel.de>
  * Author URI: https://palasthotel.de
  * Text Domain: live-news
  * Domain Path: /languages
  * Requires at least: 4.0
- * Tested up to: 5.2.3
+ * Tested up to: 5.5.3
  * License: http://www.gnu.org/licenses/gpl-3.0.html GPLv3
  *
  * @copyright Copyright (c) 2019, Palasthotel
