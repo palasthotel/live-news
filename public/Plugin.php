a2dfad2e58ea78ead1b90eee74bbb7a07cce654a
Edward <edward.bock@palasthotel.de>
2023-10-24 09:57:12 +0200

    fix namespace

--- public/Plugin.php
+++ public/Plugin.php
@@ -2,7 +2,7 @@
 /**
  *
  * Plugin Name: Live News
- * Plugin URI: https://github.com/EdwardBock/live-news
+ * Plugin URI: https://github.com/palasthotel/live-news
  * Description: Custom post type for news stream
  * Version: 1.2.0
  * Author: Palasthotel by Stephan <stephan.kroppenstedt@palasthotel.de> & Edward <edward.bock@palasthotel.de>
